dacbc57bc7e73f7e9b21b7209c10e0b398c1d5e5
Crisbern <crisberngozun022@gmail.com>
2021-09-18 23:00:38 +0800

    fixed top scrolling

--- static_files/actions.js
+++ static_files/actions.js
@@ -1,15 +1,21 @@
-const navigationItems = document.querySelectorAll('.navigation-item')
-const siteSections = document.querySelectorAll('section')
+const navigationItems = document.querySelectorAll('.navigation-item') //nav items
+const siteSections = document.querySelectorAll('section')//sections
 
 navigationItems.forEach(navigationItem => {
     let sectionId = navigationItem.dataset.sectionId
 
     navigationItem.onclick = () => {
+        // Scrolls on top on-click
+        document.body.scrollTop = 0; 
+        document.documentElement.scrollTop = 0; 
+
+        //appending and removing active class for nav items
         const currentSection = document.querySelector('.navigation-item.active')
         currentSection.classList.remove('active')
 
         navigationItem.classList.add('active')
 
+        //hide and show sections based on the pressed button
         siteSections.forEach(siteSection => {
 
             if(sectionId == siteSection.id){
@@ -20,6 +26,7 @@ navigationItems.forEach(navigationItem => {
             }
         })
 
+        //change page title
         document.title = navigationItem.dataset.pageTitle
     }
 })
